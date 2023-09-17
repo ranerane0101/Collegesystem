a7f80e09dafbb777b81d8bf029b00774c136d733
hikari <runrunrun4649@icloud.comm>
2023-09-17 18:43:20 +0900

    second commit

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ContosoUniversity.Models
+{
+	public class Course
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int CourseID { get; set; }
+		public string Title { get; set; }
+		public int Credits { get; set; }
+
+
+		public ICollection<Enrollment> Enrollments { get; set; }
+
+	}
+}
+
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ContosoUniversity.Models
+{
+	public enum Grade
+	{
+		A,B,C,D,F
+	}
+	public class Enrollment
+	{
+		public int EnrollmentID { get; set; }
+		//外部キー
+		public int CourseID { get; set; }
+		//外部キー
+		public int StudentID { get; set; }
+		public Grade? Grade {get; set;}
+		//ナビゲーションプロパティ
+		public Course Course { get; set; }
+		//ナビゲーションプロパティ
+		public Student Student { get; set; }
+	}
+}
+
