96f7d269fbafe1eb81e4ee919e88a0b1667fca92
hikari <runrunrun4649@icloud.comm>
2023-09-22 09:19:15 +0900

    MySQ:L接続

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,24 +1,29 @@
 ﻿using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
 
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = "server=localhost;user=user;password=password;database=dotnetdb";
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
+//var connectionString = "server=localhost;user=user;password=password;database=dotnetdb";
+//var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
 
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SchoolContext>(
-            dbContextOptions => dbContextOptions
-                .UseMySql(connectionString, serverVersion)
-                .LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableSensitiveDataLogging()
-                .EnableDetailedErrors()
-        );
+//builder.Services.AddDbContext<SchoolContext>(
+//            dbContextOptions => dbContextOptions
+//                .UseMySql(connectionString, serverVersion)
+//                .LogTo(Console.WriteLine, LogLevel.Information)
+//                .EnableSensitiveDataLogging()
+//                .EnableDetailedErrors()
+//        );
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
